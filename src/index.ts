e4c8f72e0ca26563c48a07777ddfeb61ada89c9d
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2026-04-24 11:20:20 -0300

    feat: add create work plan endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import "dotenv/config";
 import fastifyCors from "@fastify/cors";
 import fastifySwagger from "@fastify/swagger";
 import fastifyApiReference from "@scalar/fastify-api-reference";
+import { fromNodeHeaders } from 'better-auth/node';
 import Fastify from 'fastify';
 import { jsonSchemaTransform, serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
 
 import { WeekDay } from "./generated/prisma/enums.js";
 import { auth } from "./lib/auth.js";
+import { CreateWorkoutPlan } from "./usecases/CreateWorkoutPlan.js";
 
 const app = Fastify({
   logger: true
@@ -66,7 +68,7 @@ app.withTypeProvider<ZodTypeProvider>().route({
         workoutDays:z.array(
         z.object({
         name: z.string().trim().min(1),
-        WeekDay: z.enum(WeekDay),
+        weekDay: z.enum(WeekDay),
         isRest: z.boolean().default(false),
         estimatedDurationInSeconds: z.number().min(1),
         exercises: z.array(z.object({
@@ -87,7 +89,7 @@ app.withTypeProvider<ZodTypeProvider>().route({
         workoutDays:z.array(
         z.object({
         name: z.string().trim().min(1),
-        WeekDay: z.enum(WeekDay),
+        weekDay: z.enum(WeekDay),
         isRest: z.boolean().default(false),
         estimatedDurationInSeconds: z.number().min(1),
         exercises: z.array(z.object({
@@ -104,11 +106,46 @@ app.withTypeProvider<ZodTypeProvider>().route({
       400: z.object({
         error: z.string(),
         code: z.string()
+      }),
+      401: z.object({
+        error: z.string(),
+        code: z.string()
+      }),
+      500: z.object({
+        error: z.string(),
+        code: z.string()
       })
 },
   },
-  handler: async (request, reply) => {}
+  handler: async (request, reply) => {
+   try {
+     const session = await auth.api.getSession({
+      headers: fromNodeHeaders(request.headers)
+    })
+
+    if(!session){
+      return reply.status(401).send({
+        error: "Unauthorized",
+        code: "UNAUTHORIZED"
+      })
+    }
+    const createWorkoutPlan = new CreateWorkoutPlan();
+    const result = await createWorkoutPlan.execute({ 
+      userId: session.user.id,
+      name: request.body.name,
+      workoutDays: request.body.workoutDays,
+    })
+    return reply.status(201).send(result)
+    } catch (error) {
+    app.log.error(error)
+    return reply.status(500).send({
+      error: "Internal server error",
+      code: "INTERNAL_ERROR"
+    })
+    }
+  }
 })
+
 
 
 
